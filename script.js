fc9cdb5a7dbc7507a8ccc718ae493b94df0261c5
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-06-16 21:53:30 +0800

    created 16x16 grid of squares, added event listener to change background color when hovering on a square

---
+++ script.js
@@ -0,0 +1,11 @@
+const grid = document.querySelector(".grid");
+
+for (let i = 0; i < 256; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseenter", () => square.style.backgroundColor = "black");
+    grid.appendChild(square);
+}
+
+
