1aecb69cf2d4d2eeae724688da36aab2622f24ef
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-06-16 22:22:19 +0800

    Added event listener for button which prompts user for squares per side and then changes it while maintaining the same pixel space

--- script.js
+++ script.js
@@ -8,4 +8,23 @@ for (let i = 0; i < 256; i++){
     grid.appendChild(square);
 }
 
+const btn = document.querySelector("#grid-change");
+btn.addEventListener("click", () => {
+    let input = prompt("Enter the number of squares per side in the new grid (Limit is 100 squares)");
+    if (input > 100){
+        input = 100;
+    }
 
+    grid.innerHTML = "";
+    for (let i = 0; i < (input * input); i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = 800/input + "px";
+        square.style.height = 800/input + "px";
+
+        square.addEventListener("mouseenter", () => square.style.backgroundColor = "black");
+        grid.appendChild(square);
+    }
+
+
+})
